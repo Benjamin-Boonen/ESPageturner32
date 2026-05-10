9fb5120dc6c5df5bd6e6e45b0db2055e51fc148d
Benjamin-Boonen <tumainibenjaminboonen7@gmail.com>
2026-05-10 18:03:03 +0200

    A status light

--- main/main.ino
+++ main/main.ino
@@ -4,23 +4,30 @@ BleKeyboard bleKeyboard("PageTurner", "Benjamin", 100);
 
 const int BTN_NEXT = 37;
 const int BTN_PREV = 39;
+const int STATUS_LIGHT = 19;
+const int HOLD_PIN = 4;
 
 void setup() {
-  pinMode(BTN_NEXT, INPUT_PULLUP);
-  pinMode(BTN_PREV, INPUT_PULLUP);
+  pinMode(BTN_NEXT, INPUT);
+  pinMode(BTN_PREV, INPUT);
   bleKeyboard.begin();
+  pinMode(STATUS_LIGHT, OUTPUT);
+  digitalWrite(STATUS_LIGHT, HIGH);
 }
 
 void loop() {
   if (!bleKeyboard.isConnected()) return;
 
   if (digitalRead(BTN_NEXT) == LOW) {
-    bleKeyboard.write(KEY_RIGHT_ARROW); // or PAGE_DOWN
-    delay(200); // debounce
+    bleKeyboard.write(KEY_RIGHT_ARROW);
+    delay(200);
   }
 
   if (digitalRead(BTN_PREV) == LOW) {
     bleKeyboard.write(KEY_LEFT_ARROW);
     delay(200);
   }
+
+  delay(10);
+
 }
